f9a0bf565d44d8b7ba37ed1de27049cd94d82797
SarryXGG <SarryXGG@gmail.com>
2022-12-07 17:05:40 +0100

    Funkční hra

--- GameTry01/GameTry01/Form1.Designer.cs
+++ GameTry01/GameTry01/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -44,141 +45,196 @@
             this.button14 = new System.Windows.Forms.Button();
             this.button15 = new System.Windows.Forms.Button();
             this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Red;
             this.button1.Location = new System.Drawing.Point(13, 13);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(60, 60);
             this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Red;
             this.button2.Location = new System.Drawing.Point(79, 13);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(60, 60);
             this.button2.TabIndex = 1;
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.Red;
             this.button3.Location = new System.Drawing.Point(145, 13);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(60, 60);
             this.button3.TabIndex = 2;
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.Red;
             this.button4.Location = new System.Drawing.Point(211, 13);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(60, 60);
             this.button4.TabIndex = 3;
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.Color.Red;
             this.button5.Location = new System.Drawing.Point(13, 79);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(60, 60);
             this.button5.TabIndex = 4;
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
+            this.button6.BackColor = System.Drawing.Color.Red;
             this.button6.Location = new System.Drawing.Point(211, 79);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(60, 60);
             this.button6.TabIndex = 5;
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
+            this.button7.BackColor = System.Drawing.Color.Red;
             this.button7.Location = new System.Drawing.Point(13, 145);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(60, 60);
             this.button7.TabIndex = 6;
-            this.button7.UseVisualStyleBackColor = true;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
+            this.button8.BackColor = System.Drawing.Color.Red;
             this.button8.Location = new System.Drawing.Point(79, 79);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(60, 60);
             this.button8.TabIndex = 6;
-            this.button8.UseVisualStyleBackColor = true;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
+            this.button9.BackColor = System.Drawing.Color.Red;
             this.button9.Location = new System.Drawing.Point(145, 79);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(60, 60);
             this.button9.TabIndex = 7;
-            this.button9.UseVisualStyleBackColor = true;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
+            this.button10.BackColor = System.Drawing.Color.Red;
             this.button10.Location = new System.Drawing.Point(79, 145);
             this.button10.Name = "button10";
             this.button10.Size = new System.Drawing.Size(60, 60);
             this.button10.TabIndex = 8;
-            this.button10.UseVisualStyleBackColor = true;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
+            this.button11.BackColor = System.Drawing.Color.Red;
             this.button11.Location = new System.Drawing.Point(145, 145);
             this.button11.Name = "button11";
             this.button11.Size = new System.Drawing.Size(60, 60);
             this.button11.TabIndex = 9;
-            this.button11.UseVisualStyleBackColor = true;
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
+            this.button12.BackColor = System.Drawing.Color.Red;
             this.button12.Location = new System.Drawing.Point(211, 145);
             this.button12.Name = "button12";
             this.button12.Size = new System.Drawing.Size(60, 60);
             this.button12.TabIndex = 10;
-            this.button12.UseVisualStyleBackColor = true;
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // button13
             // 
+            this.button13.BackColor = System.Drawing.Color.Red;
             this.button13.Location = new System.Drawing.Point(13, 211);
             this.button13.Name = "button13";
             this.button13.Size = new System.Drawing.Size(60, 60);
             this.button13.TabIndex = 11;
-            this.button13.UseVisualStyleBackColor = true;
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // button14
             // 
+            this.button14.BackColor = System.Drawing.Color.Red;
             this.button14.Location = new System.Drawing.Point(79, 211);
             this.button14.Name = "button14";
             this.button14.Size = new System.Drawing.Size(60, 60);
             this.button14.TabIndex = 12;
-            this.button14.UseVisualStyleBackColor = true;
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // button15
             // 
+            this.button15.BackColor = System.Drawing.Color.Red;
             this.button15.Location = new System.Drawing.Point(145, 211);
             this.button15.Name = "button15";
             this.button15.Size = new System.Drawing.Size(60, 60);
             this.button15.TabIndex = 13;
-            this.button15.UseVisualStyleBackColor = true;
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
             // 
             // button16
             // 
+            this.button16.BackColor = System.Drawing.Color.Red;
             this.button16.Location = new System.Drawing.Point(211, 211);
             this.button16.Name = "button16";
             this.button16.Size = new System.Drawing.Size(60, 60);
             this.button16.TabIndex = 14;
-            this.button16.UseVisualStyleBackColor = true;
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(13, 278);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(258, 23);
+            this.button17.TabIndex = 15;
+            this.button17.Text = "button17";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(278, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(232, 277);
+            this.listBox1.TabIndex = 16;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(279, 282);
+            this.ClientSize = new System.Drawing.Size(522, 309);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button17);
             this.Controls.Add(this.button16);
             this.Controls.Add(this.button15);
             this.Controls.Add(this.button14);
@@ -197,6 +253,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -219,6 +276,9 @@
         private System.Windows.Forms.Button button14;
         private System.Windows.Forms.Button button15;
         private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- GameTry01/GameTry01/Form1.cs
+++ GameTry01/GameTry01/Form1.cs
@@ -16,5 +16,415 @@ namespace GameTry01
         {
             InitializeComponent();
         }
+        Random random = new Random();
+        int pocet = 0;
+        DateTime dt;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        /// <summary>
+        /// Generuje barvu na zelenou
+        /// </summary>
+        private void Generace()
+        {
+            int number = random.Next(1, 16);
+            switch (number)
+            {
+                case 1:
+                    button1.BackColor = Color.Green;
+                    break;
+                case 2:
+                    button2.BackColor = Color.Green;
+                    break;
+                case 3:
+                    button3.BackColor = Color.Green;
+                    break;
+                case 4:
+                    button4.BackColor = Color.Green;
+                    break;
+                case 5:
+                    button5.BackColor = Color.Green;
+                    break;
+                case 6:
+                    button6.BackColor = Color.Green;
+                    break;
+                case 7:
+                    button7.BackColor = Color.Green;
+                    break;
+                case 8:
+                    button8.BackColor = Color.Green;
+                    break;
+                case 9:
+                    button9.BackColor = Color.Green;
+                    break;
+                case 10:
+                    button10.BackColor = Color.Green;
+                    break;
+                case 11:
+                    button11.BackColor = Color.Green;
+                    break;
+                case 12:
+                    button12.BackColor = Color.Green;
+                    break;
+                case 13:
+                    button13.BackColor = Color.Green;
+                    break;
+                case 14:
+                    button14.BackColor = Color.Green;
+                    break;
+                case 15:
+                    button15.BackColor = Color.Green;
+                    break;
+                case 16:
+                    button16.BackColor = Color.Green;
+                    break;
+            }
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            pocet = 0;
+            timer1.Start();
+            dt = DateTime.Now;
+            Generace();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.BackColor == Color.Green)
+            {
+                button1.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (button2.BackColor == Color.Green)
+            {
+                button2.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (button3.BackColor == Color.Green)
+            {
+                button3.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.BackColor == Color.Green)
+            {
+                button4.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.BackColor == Color.Green)
+            {
+                button5.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.BackColor == Color.Green)
+            {
+                button6.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (button7.BackColor == Color.Green)
+            {
+                button7.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (button8.BackColor == Color.Green)
+            {
+                button8.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (button9.BackColor == Color.Green)
+            {
+                button9.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (button10.BackColor == Color.Green)
+            {
+                button10.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (button11.BackColor == Color.Green)
+            {
+                button11.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (button12.BackColor == Color.Green)
+            {
+                button12.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (button13.BackColor == Color.Green)
+            {
+                button13.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if (button14.BackColor == Color.Green)
+            {
+                button14.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (button15.BackColor == Color.Green)
+            {
+                button15.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (button16.BackColor == Color.Green)
+            {
+                button16.BackColor = Color.Red;
+                ++pocet;
+
+                if (pocet == 10)
+                {
+                    timer1.Stop();
+                    DateTime dt2 = DateTime.Now;
+                    TimeSpan konecnyCas = dt2 - dt;
+                    listBox1.Items.Add(konecnyCas.ToString());
+                }
+                else
+                {
+                    Generace();
+                }
+            }
+        }
     }
 }
